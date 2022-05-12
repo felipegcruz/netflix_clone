fccd4ea77eec52e2e592b13efd4679ba5af75132
Felipe <felipe@gmail.com>
2022-05-12 09:29:39 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//Select tab content item
+function selectItem(e){
+    removeBorder();
+    //Add border to current tab
+    this.classList.add('tab-border');
+}
+
+function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+
+// Listen to tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
